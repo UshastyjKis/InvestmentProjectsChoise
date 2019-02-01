a72a8d5ac3eb862306bc283df25e954ac6183e38
UshastyjKis <46782300+UshastyjKis@users.noreply.github.com>
2019-02-01 20:50:23 +0300

    Update Autorisation.java
    Fixed error (Choice).

--- Autorisation.java
+++ Autorisation.java
@@ -3,7 +3,7 @@ import java.sql.SQLException;
 public class Autorisation {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		Ñhoice ch = new Ñhoice();
+		Choice ch = new Choice();
 		ch.getDbConnection();
 		ch.fillingBase();
 		ch.makeChoice();
