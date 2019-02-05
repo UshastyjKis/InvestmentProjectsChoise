f16cf25a873f954ef0d35e42567b1e0d48b6e320
UshastyjKis <46782300+UshastyjKis@users.noreply.github.com>
2019-02-05 22:39:28 +0300

    Delete Configs.java

--- Configs.java
+++
@@ -1,8 +0,0 @@
-
-public class Configs {
-	protected String dbHost = "localhost";
-	protected String dbPort = "3306";
-	protected String dbUser = "root";
-	protected String dbPass = "031287";
-	protected String dbName = "invest_projects";
-}
